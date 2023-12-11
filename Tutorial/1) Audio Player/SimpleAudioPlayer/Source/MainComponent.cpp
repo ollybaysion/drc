307d6d0803ee74751b0fbc8501c5d5ec5af59418
Dongseong Kim <ollybaysion@naver.com>
2023-12-12 01:58:58 +0900

    #2-Simple Audio Player 구현
    - 버튼 State (Stopped / Starting / Playing / Stopping)
    - Open 버튼 구현
    - Start 버튼 구현
    - Stop 버튼 구현

--- Tutorial/1) Audio Player/SimpleAudioPlayer/Source/MainComponent.cpp
+++ Tutorial/1) Audio Player/SimpleAudioPlayer/Source/MainComponent.cpp
@@ -5,21 +5,24 @@ MainComponent::MainComponent() : state (Stopped)
 {
     addAndMakeVisible(&openButton);
     openButton.setButtonText("Open...");
-    // openButton.onClick = [this] { openButtonClicked(); };
+    openButton.onClick = [this] { openButtonClicked(); };
 
     addAndMakeVisible(&playButton);
     playButton.setButtonText("Play");
-    // playButton.onClick = [this] { playButtonClicked(); };
+    playButton.onClick = [this] { playButtonClicked(); };
     playButton.setColour(juce::TextButton::buttonColourId, juce::Colours::green);
     playButton.setEnabled(false);
 
     addAndMakeVisible(&stopButton);
     stopButton.setButtonText("Stop");
-    // stopButton.onClick = [this] { stopButtonClicked(); };
+    stopButton.onClick = [this] { stopButtonClicked(); };
     stopButton.setColour(juce::TextButton::buttonColourId, juce::Colours::red);
     stopButton.setEnabled(false);
 
     setSize(300, 200);
+
+    formatManager.registerBasicFormats();   // Basic Format = .wav .aiff
+    transportSource.addChangeListener(this);
 
     setAudioChannels(0, 2);
 }
@@ -33,24 +36,18 @@ MainComponent::~MainComponent()
 //==============================================================================
 void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
 {
-    // This function will be called when the audio device is started, or when
-    // its settings (i.e. sample rate, block size, etc) are changed.
-
-    // You can use this function to initialise any resources you might need,
-    // but be careful - it will be called on the audio thread, not the GUI thread.
-
-    // For more details, see the help for AudioProcessor::prepareToPlay()
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
 }
 
 void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    // Your audio-processing code goes here!
-
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
-    bufferToFill.clearActiveBufferRegion();
+    if (readerSource.get() == nullptr)
+    {
+        bufferToFill.clearActiveBufferRegion();
+        return;
+    }
+    
+    transportSource.getNextAudioBlock(bufferToFill);
 }
 
 void MainComponent::releaseResources()
@@ -75,4 +72,54 @@ void MainComponent::resized()
     openButton.setBounds(10, 10, getWidth() - 20, 20);
     playButton.setBounds(10, 40, getWidth() - 20, 20);
     stopButton.setBounds(10, 70, getWidth() - 20, 20);
+}
+
+void MainComponent::changeListenerCallback(juce::ChangeBroadcaster* source)
+{
+    if (source == &transportSource)
+    {
+        if (transportSource.isPlaying())
+            changeState(Playing);
+        else
+            changeState(Stopped);
+    }
+}
+
+void MainComponent::openButtonClicked()
+{
+    chooser = std::make_unique<juce::FileChooser>("Select a wav File to Play...",
+        juce::File{},
+        "*.wav");
+
+    auto chooserFlags = juce::FileBrowserComponent::openMode
+        | juce::FileBrowserComponent::canSelectFiles;
+
+    chooser->launchAsync(chooserFlags, [this](const juce::FileChooser& fc)
+        {
+            auto file = fc.getResult();
+
+            if (file != juce::File{})
+            {
+                auto* reader = formatManager.createReaderFor(file);
+
+                if (reader != nullptr)
+                {
+                    auto newSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+                    transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
+                    playButton.setEnabled(true);
+                    readerSource.reset(newSource.release());
+                }
+            }
+        }
+    );
+}
+
+void MainComponent::playButtonClicked()
+{
+    MainComponent::changeState(Starting);
+}
+
+void MainComponent::stopButtonClicked()
+{
+    MainComponent::changeState(Stopping);
 }
--- Tutorial/1) Audio Player/SimpleAudioPlayer/Source/MainComponent.h
+++ Tutorial/1) Audio Player/SimpleAudioPlayer/Source/MainComponent.h
@@ -7,7 +7,7 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::AudioAppComponent
+class MainComponent  : public juce::AudioAppComponent, public juce::ChangeListener
 {
 public:
     //==============================================================================
@@ -22,6 +22,7 @@ public:
     //==============================================================================
     void paint (juce::Graphics& g) override;
     void resized() override;
+    void changeListenerCallback(juce::ChangeBroadcaster* source) override;
 
 private:
     //==============================================================================
@@ -35,12 +36,50 @@ private:
         Stopping
     };
 
+    void changeState(TransportState newState)
+    {
+        if (state != newState)
+        {
+            state = newState;
+
+            switch (state)
+            {
+            case Stopped:
+                stopButton.setEnabled(false);
+                playButton.setEnabled(true);
+                transportSource.setPosition(0.0);
+                break;
+
+            case Starting:
+                playButton.setEnabled(false);
+                transportSource.start();
+                break;
+
+            case Playing:
+                stopButton.setEnabled(true);
+                break;
+
+            case Stopping:
+                transportSource.stop();
+                break;
+            }
+        }
+    }
+
+    void openButtonClicked();
+    void playButtonClicked();
+    void stopButtonClicked();
+
     juce::TextButton openButton;
     juce::TextButton playButton;
     juce::TextButton stopButton;
 
-    int state;
+    std::unique_ptr<juce::FileChooser> chooser;
 
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+    int state;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
